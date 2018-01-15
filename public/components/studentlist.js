b5975ecfd08a0cd37d0090e0f75724bc2373f0e3
suiffet guillaume <suiffet.g@gmail.com>
2018-01-16 00:53:03 +0100

    accept config production

--- public/components/studentlist.js
+++ public/components/studentlist.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var connect   = require('react-redux').connect;
+
 var UpdateStudentXForm = require('./updatestudent.js');
 
 class StudentList extends React.Component {
